272d66dbbf4e07eaca947efdfbb3ba25afadb22e
Lertyy <lertyy1337@gmail.com>
2025-11-15 21:51:50 +0200

    add all

--- js/task-3.js
+++
@@ -1,6 +0,0 @@
-const inputEl = document.querySelector("#name-input");
-const outputEl = document.querySelector("#name-output");
-inputEl.addEventListener("input", (Event) => {
-  const trimmedValue = Event.currentTarget.value.trim();
-  outputEl.textContent = trimmedValue === "" ? "Anonymous" : trimmedValue;
-});
